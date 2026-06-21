27660992643cd30bf7f58fa3b242a7f8083c0e78
Madhav-blip <madhavprakash06@gmail.com>
2026-06-21 14:03:12 +0530

    Day 13 Modular Heading

---
+++ day13/react-lec-1/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import  {Head} from "./Heading.jsx"
+
+function App() {
+
+  return (
+    <>
+    <Head name='Madhav' />
+    <div classname="example1">This is div example-1</div>
+    </>
+  )
+}
+
+export default App
+// React is a library and not a framework.
---
+++ day13/react-lec-1/src/Heading.jsx
@@ -0,0 +1,12 @@
+export function Head({name}){
+  return(
+    <div>
+     <h1>Heading 1 - by {name}</h1>
+     <br />
+     <h2>Aspiring Software Engineer</h2>
+     <br />
+     <h2>Currently pusuing training Program @Zenmonk</h2>
+     <br />
+     </div>
+  )
+}
